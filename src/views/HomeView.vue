157e21080c6ed10c4b918ed9a9d477cd550739ee
Matheus Rodrigues <joaoomatheus@hotmail.com>
2022-07-31 11:49:04 -0300

    init configurations #1

---
+++ src/views/HomeView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>working</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  setup() {
+    return {};
+  },
+});
+</script>
+
+<style scoped></style>
