5751f52e09edb47946e9b0d86691f5ac6cbe5f28
Matheus Rodrigues <joaoomatheus@hotmail.com>
2022-07-31 13:42:05 -0300

    Added the Paginator component #3

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,14 +1,19 @@
 <template>
-  <div class="container">working</div>
+  <div class="container">
+    <h1>Pokedex</h1>
+    <Paginator :page="1" />
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
+import Paginator from "../components/Paginator.vue";
 
 export default defineComponent({
   setup() {
     return {};
   },
+  components: { Paginator },
 });
 </script>
 
