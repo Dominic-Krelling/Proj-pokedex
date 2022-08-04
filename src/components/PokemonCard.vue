f6a9daed5406fb8fa771661c5f0ad0c860c4c4f1
Matheus Rodrigues <joaoomatheus@hotmail.com>
2022-08-03 21:34:54 -0300

    Showing pokemons on screen #4

---
+++ src/components/PokemonCard.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="pokemon-card-container" :style="{ background: background }">
+    <h3>{{ pokemonName }}</h3>
+    <img :src="pokemonImage" alt="pokemon image" class="pokemon-image" />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  props: {
+    pokemonName: {
+      type: String,
+      required: true,
+    },
+    pokemonImage: {
+      type: String,
+    },
+    pokemonType: {
+      type: String,
+    },
+  },
+  setup() {
+    return {};
+  },
+  computed: {
+    background() {
+      return "#fff";
+    },
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+.pokemon-card-container {
+  height: 300px;
+  width: 120px;
+}
+
+.pokemon-image {
+  max-width: 100%;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,16 @@
 <template>
   <div class="container">
     <h1>Pokedex</h1>
+    <section v-if="pokemons.length > 0" class="pokemon-section">
+      <PokemonCard
+        v-for="(pokemon, index) in pokemons"
+        :pokemonName="pokemon.name"
+        :pokemonImage="pokemon.sprites.other['official-artwork'].front_default"
+        :key="pokemon.name"
+      />
+      <!-- {{ pokemon.sprites.other["official-artwork"] }} -->
+      <!-- :pokemonType="types[0].type.name" -->
+    </section>
     <Paginator :page="1" />
   </div>
 </template>
@@ -8,13 +18,48 @@
 <script lang="ts">
 import { defineComponent } from "vue";
 import Paginator from "../components/Paginator.vue";
+import PokemonCard from "../components/PokemonCard.vue";
+import axios from "axios";
 
 export default defineComponent({
   setup() {
     return {};
   },
-  components: { Paginator },
+  components: { Paginator, PokemonCard },
+  methods: {
+    async getPokemon(pokemonId: Number) {
+      const result = await axios.get(
+        "https://pokeapi.co/api/v2/pokemon/" + pokemonId
+      );
+      return result;
+    },
+    async getPagePokemons() {
+      for (let i = this.page; i <= this.page * this.pokemonsPerPage; i++) {
+        await this.getPokemon(i).then((res) => {
+          console.log(res.data.sprites.other["official-artwork"].front_default);
+          this.pokemons.push(res.data);
+        });
+      }
+      console.log(this.pokemons);
+    },
+  },
+  data() {
+    return {
+      pokemons: [],
+      pokemonsPerPage: 10,
+      page: 1,
+    };
+  },
+  mounted() {
+    this.getPagePokemons();
+  },
 });
 </script>
 
-<style scoped></style>
+<style lang="scss" scoped>
+.pokemon-section {
+  width: 100%;
+  display: flex;
+  gap: 1rem;
+}
+</style>
